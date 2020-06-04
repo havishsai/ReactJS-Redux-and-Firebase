b599be3f39b079056a9a44ea10757a1adf8b054f
havishsai <34995850+havishsai@users.noreply.github.com>
2020-06-04 19:46:56 +0530

    Update fbConfig.js

--- reactreduxfirebaseproject/src/config/fbConfig.js
+++ reactreduxfirebaseproject/src/config/fbConfig.js
@@ -3,14 +3,14 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAqBD3zqpNxmHDYJM1OehpLkxoPNFP4Sa4",
-    authDomain: "projectlistreact.firebaseapp.com",
-    databaseURL: "https://projectlistreact.firebaseio.com",
-    projectId: "projectlistreact",
-    storageBucket: "projectlistreact.appspot.com",
-    messagingSenderId: "246588004471",
-    appId: "1:246588004471:web:c7f8da6b1d85cc04f44e72",
-    measurementId: "G-SVMDN78D3G"
+    apiKey: "*****************************",
+    authDomain: "***************************",
+    databaseURL: "************************",
+    projectId: "***********************",
+    storageBucket: "*********************************",
+    messagingSenderId: "************************************",
+    appId: "**************************************",
+    measurementId: "***********************************"
   };
 
   firebase.initializeApp(firebaseConfig);
